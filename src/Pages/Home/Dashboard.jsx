bfb99b7c850c5b5b03c94708e842aa7812cd80ca
binayakbishnu <bishnu.binayak12@gmail.com>
2023-08-26 13:18:30 +0530

    feat: added backend endpoints for api, 2-way comm

--- src/Pages/Home/Dashboard.jsx
+++ src/Pages/Home/Dashboard.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import axios from 'axios';
+
 function Dashboard() {
-    const [city, setCity] = useState("Delhi");
+    const [city, setCity] = useState("");
     const cityInput = React.createRef();
 
     const [error, setError] = useState(true);
@@ -14,28 +16,60 @@ function Dashboard() {
         console.log(cityInput.current?.value);
         setCity(cityInput.current?.value);
 
-        await getWeatherDetails();
+        await sendReceiveByAxios();
+
+        // setDisplay();
     }
 
-    const [weatherDetails, setWeatherDetails] = useState(null);
-    const getWeatherDetails = async () => {
-        setWeatherDetails(
-            {
-                "currentTemperature": 32,
-                "description": "sunny",
-                "windSpeed": 0,
-                "humidity": "80%",
-                // other info
-            }
-        )
+    const [receivedOutput, setReceivedOutput] = useState("x");
+    const sendReceiveByAxios = async () => {
+        try {
+            await axios.post(`http://localhost:8000/sendCityName`, {
+                name: city,
+            }).then(res => {
+                console.log(`${res.status != 200 ? 'from backend: Backend error' : `from backend: ${res.data}`}`);
+                if (res.data.city === city) {
+                    // all was okay
+                    setReceivedOutput(res.data);
+                    console.log("all okay");
+                }
+                else {
+                    console.warn(`not: same: ${city} ${res.data.city}`);
+                }
+            }).catch(e => {
+                console.log(`frontend: axios error: ${e}`)
+            }).finally(() => {
+                console.log('frontend: axios completed successfully');
+            })
+        }
+        catch (e) {
+            console.log(`frontend: ${e}`);
+        }
     }
+
+    const [weatherDetails, setWeatherDetails] = useState("x");
+    const setDisplay = () => {
+        if (receivedOutput === "x") return;
+        console.log(receivedOutput);
+        setWeatherDetails({
+            city: receivedOutput.city,
+            currentTemperature: receivedOutput.temp,
+            description: receivedOutput.description,
+            windSpeed: receivedOutput.windSpeed,
+            humidity: receivedOutput.humidity,
+        })
+    }
+
+    useEffect(() => {
+        setDisplay();
+    }, [receivedOutput]);
 
     return (
         <div className='flex-1 p-2 bg-[rgba(0,0,25,1.0)] text-white relative'>
             <div className='mainContent flex flex-col items-stretch justify-start'>
                 <div className={`inputDiv`}>
-                    <form /* action="/sendCityName" */ onSubmit={submitCity} className={`lg:w-[30%] m-auto flex flex-row gap-5 justify-between items-center`}>
-                        <input type="text" list="cities" placeholder='Start typing...' ref={cityInput}
+                    <form action="/sendCityName" onSubmit={submitCity} className={`lg:w-[30%] m-auto flex flex-row gap-5 justify-between items-center`}>
+                        <input onChange={(e) => setCity(e.target.value)} type="text" list="cities" placeholder='Start typing...' ref={cityInput}
                             className={`bg-[rgba(0,0,0,0)] px-6 py-4 border border-white rounded-xl`} />
                         <datalist id="cities">
                             <option value="Kolkata" />
@@ -51,13 +85,16 @@ function Dashboard() {
                 </div>
 
                 <div className={`outputDiv mt-[4%]`}>
-                    {error &&
+                    <p className='text-white'>sent: {city}</p>
+                    {
+                        error &&
                         <p className={`text-red-500`}>{errorMessage}</p>
                     }
 
                     <div className={`outputContent mt-[2%] w-[90%] m-auto bg-[rgba(0,0,200,1.0)] rounded-xl min-h-[20vh] p-5`}>
-                        <p className='text-white'>{city}</p>
-                        {weatherDetails &&
+                        <p className='text-white'>received: {weatherDetails.city}</p>
+                        {
+                            weatherDetails &&
                             <div className={`w-full text-left ms-5`}>
                                 <ul>
                                     <li>Current Temperature: {weatherDetails.currentTemperature}</li>
